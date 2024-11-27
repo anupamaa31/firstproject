cd33f523ff1965829be7d1cbc03a9fc6a50cb607
anupamaa31 <anupamakoduru.731>
2024-11-27 12:23:26 +0530

    heyyyy

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # firstproject
+This is my first Git Repository.
+<br>
+Author - Anupama.k
+this is anu
+hey
---
+++ task.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+int main() {
+    std::vector<int> vec;
+    vec.push_back(10);
+    vec.push_back(20);
+    vec.push_back(30);
+ 
+    for (size_t i = 0; i < vec.size(); ++i) {
+        std::cout << "Element at index " << i << ": " << vec[i] << std::endl;
+    }
+    vec.pop_back();
+    std::cout << "After pop_back, size: " << vec.size() << std::endl;
+    return 0;
+}
